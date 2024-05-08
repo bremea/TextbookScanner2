7f968d6cf7d3f8b3b0d5eb8b54ededd47895bcf2
bremea <bremeagames@gmail.com>
2024-05-08 11:31:43 -0400

    add textbook and course typings

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,6 +4,32 @@ export interface StudentData {
 	id: string;
 }
 
+export interface Student extends StudentData {
+	courses: Course[];
+}
+
+export interface Course extends CourseData {
+	textbook: TextbookData;
+}
+
+export interface TextbookData {
+	id: number;
+	name: string;
+	isbn13?: number;
+	barcode?: number;
+	course: number;
+}
+
+export interface CourseData {
+	id: number;
+	name: string;
+}
+
+export interface StudentCourses {
+	studentId: number;
+	courseId: number;
+}
+
 export interface TokenData {
 	name: string;
 }
