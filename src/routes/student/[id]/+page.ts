e78a5d05130d13e5886f84402eaa130fd710d812
bremea <bremeagames@gmail.com>
2024-05-08 11:09:48 -0400

    fetch student data on student page load

--- src/routes/student/[id]/+page.ts
+++ src/routes/student/[id]/+page.ts
@@ -1,9 +1,14 @@
 import type { StudentData } from '$lib/types.js';
+import { error } from '@sveltejs/kit';
 
-export const load = ({ params }): StudentData => {
-	return {
-		firstName: "Brett",
-		lastName: "Meadows",
-		id: params.id
-	};
+export const load = async ({ params }): Promise<StudentData> => {
+	const req = await fetch(`/api/student/${params.id}`);
+
+	if (req.status == 200) {
+		const data = (await req.json()) as StudentData;
+
+		return data;
+	} else {
+		error(req.status, await req.text());
+	}
 };
