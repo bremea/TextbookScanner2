4a88c926e0e193075a7f543ffe3cae78156cde67
bremea <bremeagames@gmail.com>
2024-05-08 12:54:21 -0400

    fix popup formatting

--- src/lib/components/misc/Popup.svelte
+++ src/lib/components/misc/Popup.svelte
@@ -8,7 +8,7 @@
 	class="top-0 left-0 z-40 flex items-center justify-center w-screen h-full absolute bg-black bg-opacity-15 backdrop-blur-sm"
 >
 	<div
-		class="max-h-screen overflow-auto z-50 absolute bg-white border-black border-2 py-4 px-8 shadow-lg pr-16"
+		class="max-h-screen overflow-auto z-50 absolute bg-white border-black border-2 py-4 px-8 shadow-lg pr-16 flex items-center justify-center text-center"
 	>
 		<button on:click={close}>
 			<Close class="absolute right-2 top-2 w-8 h-8 hover:scale-105 active:scale-95" />
