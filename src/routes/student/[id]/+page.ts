50bd6bdebd9e5f3d02643c9eada82078bc987a88
bremea <bremeagames@gmail.com>
2024-05-08 11:13:06 -0400

    add authentication to student page

--- src/routes/student/[id]/+page.ts
+++ src/routes/student/[id]/+page.ts
@@ -1,8 +1,12 @@
 import type { StudentData } from '$lib/types.js';
 import { error } from '@sveltejs/kit';
 
+export const ssr = false;
+
 export const load = async ({ params }): Promise<StudentData> => {
-	const req = await fetch(`/api/student/${params.id}`);
+	const req = await fetch(`/api/student/${params.id}`, {
+		headers: { Authentication: localStorage.getItem('token')! }
+	});
 
 	if (req.status == 200) {
 		const data = (await req.json()) as StudentData;
