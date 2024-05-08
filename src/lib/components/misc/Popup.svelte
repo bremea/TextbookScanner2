6e6aac2bb6681ce5c3e4ccb28c7f8ff455e76b9d
bremea <bremeagames@gmail.com>
2024-05-08 12:27:48 -0400

    scroll in popup

--- src/lib/components/misc/Popup.svelte
+++ src/lib/components/misc/Popup.svelte
@@ -5,9 +5,11 @@
 </script>
 
 <div
-	class="z-40 flex items-center justify-center w-screen h-full absolute bg-black bg-opacity-15 backdrop-blur-sm"
+	class="top-0 left-0 z-40 flex items-center justify-center w-screen h-full absolute bg-black bg-opacity-15 backdrop-blur-sm"
 >
-	<div class="z-50 absolute bg-white border-black border-2 py-4 px-8 shadow-lg pr-16">
+	<div
+		class="max-h-screen overflow-auto z-50 absolute bg-white border-black border-2 py-4 px-8 shadow-lg pr-16"
+	>
 		<button on:click={close}>
 			<Close class="absolute right-2 top-2 w-8 h-8 hover:scale-105 active:scale-95" />
 		</button>
