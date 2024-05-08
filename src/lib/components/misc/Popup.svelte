d8d2d5f93ced07833d601af090eb0d22e94d737a
bremea <bremeagames@gmail.com>
2024-05-08 12:23:08 -0400

    add spacing for X on popup

--- src/lib/components/misc/Popup.svelte
+++ src/lib/components/misc/Popup.svelte
@@ -7,7 +7,7 @@
 <div
 	class="z-40 flex items-center justify-center w-screen h-full absolute bg-black bg-opacity-15 backdrop-blur-sm"
 >
-	<div class="z-50 absolute bg-white border-black border-2 py-4 px-8 shadow-lg">
+	<div class="z-50 absolute bg-white border-black border-2 py-4 px-8 shadow-lg pr-16">
 		<button on:click={close}>
 			<Close class="absolute right-2 top-2 w-8 h-8 hover:scale-105 active:scale-95" />
 		</button>
