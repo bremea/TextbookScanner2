7486ee886aa50c893dabb2fd946d945f5816e4e9
bremea <bremeagames@gmail.com>
2024-05-08 10:49:13 -0400

    student lookup

--- src/routes/api/lookup/+server.ts
+++ src/routes/api/lookup/+server.ts
@@ -1,11 +1,41 @@
 import { verifyToken } from '$lib/server/authentication.js';
+import { pool } from '$lib/server/database.js';
 import { error, json } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+import type { RowDataPacket } from 'mysql2/promise';
 
-/** @type {import('./$types').RequestHandler} */
-export async function GET({ request }) {
+export const GET: RequestHandler = async ({ request, url }) => {
 	if (!verifyToken(request.headers)) {
 		return error(403, 'Authentication failed');
 	}
 
-	return json({ studentFound: true, id: 243780 });
-}
+	const id = url.searchParams.get('id');
+	const lastName = url.searchParams.get('lastName');
+
+	if (id) {
+		const [results] = await pool.query<RowDataPacket[]>('SELECT * FROM students WHERE id = ?', [
+			id
+		]);
+
+		if (results.length == 0) {
+			return error(404, `No student found with ID ${id}`);
+		} else {
+			return json(results);
+		}
+	} else {
+		if (!lastName) {
+			return error(400, 'Missing id or last name');
+		}
+
+		const [results] = await pool.query<RowDataPacket[]>(
+			'SELECT * FROM students WHERE LOWER(lastName) = LOWER(?)',
+			[lastName]
+		);
+
+		if (results.length == 0) {
+			return error(404, `No student found with last name "${lastName}"`);
+		} else {
+			return json(results);
+		}
+	}
+};
